4c8cdc76e42046247269139756ae27fa70d438ff
roger <rogerandrecampos@gmail.com>
2026-05-10 18:46:49 -0300

    aaa

--- JOGAR_RPG.py
+++ JOGAR_RPG.py
@@ -8,22 +8,27 @@ from model.enums import Tipo_item
 from model.geral import *
 from tkinter import messagebox
 
-# criar listas de missões e personagens para o jogo
+#help(mostrar_lista) # teste de documentação
+#pausa()
+
+# criar listas de missões, personagens e itens para o jogo
 lista_Personagens: list[Personagem] = []
 lista_Missoes: list[Missao] = []
+lista_itens: list[Item] = []
 
+#itens default
+lista_itens.append(Item("Espada de Edward", "espada que pertencia a um antigo pirata", 40, Tipo_item.ARMA))
+lista_itens.append(Item("Capacete de Comerciante", "capacete formal usado por comerciantes", 10, Tipo_item.VESTIMENTA))
+lista_itens.append(Item("Espelho Miraculoso", "espelho que aumenta a vida de quem o equipa", 30, Tipo_item.UTILITARIO))
 
-#msco = MissaoColeta("Coletar maças", "Colete maças pela montanha de olindo", 40,"Macas", 10)
-#msco2 = MissaoColeta("Coletar maças", "Colete maças pela montanha de olindo", 40,"Macas", 10)
-#mscomb1 = MissaoCombate("Derrotar Goblin", "Derrote os goblins da arena", 40, 50,"Goblin")
-item_1 = Item("Espada de Edward", "espada que pertencia a um antigo pirata", 40, Tipo_item.ARMA)
-item_2 = Item("Capacete de Comerciante", "capacete formal usado por comerciantes", 10, Tipo_item.VESTIMENTA)
-item_3 = Item("Espelho Miraculoso", "espelho que aumenta a vida de quem o equipa", 30, Tipo_item.UTILITARIO)
-
+#missoes default
 lista_Missoes.append(MissaoColeta("Coletar maças", "Colete maças pela montanha de olindo", 40,"Macas", 10))
 lista_Missoes.append(MissaoCombate("Derrotar Goblin", "Derrote os goblins da arena", 40, 50,"Goblin"))
 
-ps = Personagem("Milena")
+# personagens default
+lista_Personagens.append(Personagem("Chitãozinho"))
+lista_Personagens.append(Personagem("Xororó"))
+lista_Personagens.append(Personagem("Zé Ramalho"))
 
 menu = 1
 while menu != 0:
@@ -31,51 +36,40 @@ while menu != 0:
     menu = int(input(
         f"0 - sair\n"
         f"1 - Criar Personagem\n"
-        f"2 - Mostrar Personagens\n"
-        f"3 - listar Missões\n"
-        f"4 - Mostrar Inventário\n"
+        f"2 - listar e detalhar Personagens\n"
+        f"3 - listar e atribuir Missões\n"
+        f"4 - listar e adicionar Itens à inventário\n"
+        f"5 - Jogar Missão\n"
         f"Escolha: "
     ))
     match menu:
         case 0:
             print("\nSaindo...\n")
         case 1:
-            nome_personagem = input("Digite o nome do novo personagem a ser criado: ")
-            novo_personagem = Personagem(nome_personagem)
-            lista_Personagens.append(novo_personagem)
+            lista_Personagens.append(Personagem(input("Digite o nome do novo personagem a ser criado: ")))
         case 2:
-           msg = f"Personagens:\n"
-           for i, valor in enumerate(lista_Personagens):
-                msg += f"{i+1} - {valor}\n"
-           print(msg)
+           print(mostrar_lista(lista_Personagens, "Personagens", 1)) # mostra a lista de personagens, conta a partir de 1
+           sel = int(input("Digite um numero da lista para detalhar, 0 para sair: "))# input para detalhar
+           if sel == 0:
+               continue
+           try:
+               print(Personagem.exibir_dados(lista_Personagens[sel-1]))
+           except IndexError:
+               print("Não encontrado personagem correspondente ao número digitado")
         case 3:
-            msg = f"Misões Disponíveis:\n"
-            for i, valor in enumerate(lista_Missoes):
-                    msg += f"{i+1} - {valor}\n"
-            print(msg)
+            print(mostrar_lista(lista_Missoes, "Missões", 1))
             pausa()
-            messagebox.askquestion("diálogo", "Atribuir Misão a algum personagem?")
+            messagebox.askquestion("Atribuição", "Atribuir Misão a algum personagem?")
         case 4:
+            print(mostrar_lista(lista_itens, "Itens Disponíveis", 1))
+        case 5:
             pass
-        case 6:
-            try:
-                ps.concluir_missao(msco, 9)
-                ps.concluir_missao(mscomb1, 50)
-            except ValueError as e:
-                print(e)
-        case 7:
-            print(ps.add_item(item_1))
-            print(ps.add_item(item_2))
-            print(ps.add_item(item_3))
-            print(ps.equipar_itens())
-            print(ps.mostrar_itens_equipados())
-        case 8:
-            print(ps.mostrar_inventario())
-        case 9:
-            print(ps.listar_Missao())
         case _:
             print(f"Opção {menu} inválida! tente novamente")
     pausa()
 
+'''
+print(ps.mostrar_itens_equipados())
+print(ps.equipar_itens())
 
-
+'''
--- model/geral.py
+++ model/geral.py
@@ -1,25 +1,55 @@
 '''
-Aqui ficam funções usadas em vários lugares:
+Aqui ficam funções usadas no jogo:
 '''
 import os 
 
 def limpar_terminal():
-    '''
-    Função para limpar o terminal, funciona tanto em windows como em mac:
-    '''
-    os.system('cls' if os.name == 'nt' else 'clear')# 'nt' é Windows, 'posix' é Linux/MacOS
+    '''Função para limpar o terminal, funciona tanto em windows como em mac:'''
+    os.system('cls' if os.name == 'nt' else 'clear')# 'nt': Windows, 'posix': Linux/MacOS
 
 def aumenta_porcentagem(valor_original, aumento):
     '''
     Calcular aumento de porcentagem em cima de um valor:
+
+    Args:
+        valor_original (int, float): valor.
+        aumento (int, float): porcentagem de aumento.
+
+    Returns:
+        float: valor + porcentagem.
     '''
     return valor_original + (valor_original * aumento / 100)
 
 def diminui_porcentagem(valor_original, diminuicao):
     '''
     Calcular desconto de porcentagem em cima de um valor:
+
+    Args:
+        valor_original (int, float): valor.
+        diminuicao (int, float): porcentagem de redução.
+
+    Returns:
+        float: valor - porcentagem.
     '''
     return valor_original - (valor_original * diminuicao / 100)
 
 def pausa():
+    '''função para pausar o terminal e exigir pressionamento de tecla:'''
     os.system('pause')
+
+def mostrar_lista(lista_origem: list, texto_saida = "Lista", iniciar_em = 0):
+    '''
+    funcão que retorna os itens de uma lista com o título:
+
+    Args:
+        lista_origem (list): uma lista de qualquer tipo.
+        texto_saida (str): Título que vai ser exibido antes, default "Lista".
+        iniciar_em (int): por onde vai começar a contar, default 0.
+
+    Returns:
+        str: título seguido da lista.
+    '''
+    msg = f"\n{texto_saida}:\n"
+    for i, valor in enumerate(lista_origem, iniciar_em):
+        msg += f"{i} - {valor}\n"
+    return msg
